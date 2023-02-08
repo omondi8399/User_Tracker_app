85ce88c81a3f41d93520ee6490b60e7b7a7d582e
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-02-08 11:19:00 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import AddUser from './Components/Users/AddUser';
 
 
 function App() {
   return (
     <div>
-
+      <AddUser />
     </div>
   );
 }
---
+++ src/Components/Users/AddUser.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Card from '../UI/Card'
+import classes from './AddUser.module.css'
+
+const AddUser = props => {
+    const AddUserHandler = (event) => {
+        event.preventDefault()
+    }
+
+    return (
+    <Card className={classes.input}>
+        <form onSubmit={AddUserHandler}>
+        <label htmlFor='username'> Username</label>
+        <input id='username' type='text' />
+        <label htmlFor='age'> Age(Years)</label>
+        <input id='age' type='number' />
+        <button type='submit'>Add User</button>
+    </form>
+    </Card>
+    )
+}
+
+export default AddUser
